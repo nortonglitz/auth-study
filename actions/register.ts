ee8692df224c20529e908b09db8c4c1f078cbcf8
Norton Glitz <norton.glitz@gmail.com>
2024-01-23 14:27:47 -0300

    create user working

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,10 @@
 "use server"
 
 import * as z from "zod"
+import { db } from "@/lib/db"
 
 import { RegisterSchema } from "@/schemas"
+import bcrypt from "bcrypt"
 
 interface RegisterReturn {
     type: "error" | "success"
@@ -10,17 +12,52 @@ interface RegisterReturn {
 }
 
 export const register = async (values: z.infer<typeof RegisterSchema>): Promise<RegisterReturn> => {
-    const validatedFields = await RegisterSchema.safeParseAsync(values)
+    try {
+        const validatedFields = await RegisterSchema.safeParseAsync(values)
 
-    if (!validatedFields.success) {
+        if (!validatedFields.success) {
+            return {
+                type: "error",
+                message: "Invalid fields."
+            }
+        }
+
+        const { email, name, password } = validatedFields.data
+
+        const userExists = await db.user.findUnique({ where: { email: email } })
+
+        if (userExists) {
+            return {
+                type: "error",
+                message: "Email already taken."
+            }
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        await db.user.create({
+            data: {
+                email: email,
+                name: name,
+                password: hashedPassword
+            }
+        })
+
+        //TODO: Send verification token email
+
+
+        return {
+            type: "success",
+            message: "User created."
+        }
+
+    } catch (err: any) {
+
         return {
             type: "error",
-            message: "Invalid fields."
+            message: "Internal error."
         }
+
     }
 
-    return {
-        type: "success",
-        message: "Email sent"
-    }
 }
